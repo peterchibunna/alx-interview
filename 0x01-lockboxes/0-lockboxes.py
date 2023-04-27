43276eded6e9bd95021de520d8cd6ce0bb3e9f29
Peter Chibunna <peterchibunna@gmail.com>
2023-04-27 18:25:47 +0100

    Try #2

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,20 +8,22 @@ def canUnlockAll(boxes):
     """Write a method that determines if all the `boxes` can be opened.
     """
     keys = []  # a set that contains the keys found
-    keys.extend(boxes[0])  # the first box is always open
-
+    # keys.extend(boxes[0])  # the first box is always open
+    keys.append(0)  # the first box is always open
+    keys.extend(boxes[0])
     can_open = []
     for key, box in enumerate(boxes):
-
+        print(key, keys, box, key in keys)
         if key == 0:
+            # keys.extend(box)
             can_open.append(True)
             continue
         else:
-            # print(key, keys, box)
             if key in keys:
                 can_open.append(True)
                 keys.extend(box)
             else:
                 can_open.append(False)
+            continue
     # print(keys)
     return False if False in can_open else True
