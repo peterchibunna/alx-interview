add1e0a651295d8a4d804bdfc2ab6886d8c8e894
Peter Chibunna <peterchibunna@gmail.com>
2023-05-13 15:54:07 +0100

    Stop iteration

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,6 +5,14 @@ Module: 0x03. Log Parsing
 import sys
 
 
+def print_stats(total_file_size, status_codes):
+    """Print the stats gathered
+    """
+    print('File size: {}'.format(total_file_size))
+    for k, v in status_codes.items():
+        print('{}: {}'.format(k, v))
+
+
 if __name__ == "__main__":
     total_file_size = 0
     status_codes = {
@@ -22,6 +30,7 @@ if __name__ == "__main__":
         while True:
             for line in sys.stdin:
                 line_count += 1
+
                 parts = line.split(' ')
                 if len(parts) == 9:
                     file_size = parts[8]
@@ -29,11 +38,9 @@ if __name__ == "__main__":
                     code = parts[7]
                     status_codes[code] += 1
                 if line_count % 10 == 0:
-                    print('File size: {}'.format(total_file_size))
-                    for k, v in status_codes.items():
-                        print('{}: {}'.format(k, v))
+                    print_stats(total_file_size, status_codes)
             sys.stdin.flush()
-    except (KeyboardInterrupt, EOFError):
-        print('File size: {}'.format(total_file_size))
-        for k, v in status_codes.items():
-            print('{}: {}'.format(k, v))
+            print_stats(total_file_size, status_codes)
+            break
+    except (KeyboardInterrupt, EOFError, StopIteration):
+        print_stats(total_file_size, status_codes)
