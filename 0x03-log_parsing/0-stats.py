58267495083e0a1cfa0f68ffc4c86058ae1e7a45
Peter Chibunna <peterchibunna@gmail.com>
2023-05-13 15:20:20 +0100

    Task #0

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Module: 0x03. Log Parsing
+"""
+import sys
+
+
+if __name__ == "__main__":
+    total_file_size = 0
+    status_codes = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0
+    }
+    line_count = 0
+    try:
+        while True:
+            for line in sys.stdin:
+                line_count += 1
+                parts = line.split(' ')
+                if len(parts) == 9:
+                    file_size = parts[8]
+                    total_file_size += int(file_size)
+                    code = parts[7]
+                    status_codes[code] += 1
+                if line_count % 10 == 0:
+                    print('File size: {}'.format(total_file_size))
+                    for k, v in status_codes.items():
+                        print('{}: {}'.format(k, v))
+            sys.stdin.flush()
+    except Exception:
+        pass
