9d90657c8bf22bc3cd97d611d1751307b5346435
Peter Chibunna <peterchibunna@gmail.com>
2023-05-13 16:50:18 +0100

    Use regex to match inputs

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,6 +2,7 @@
 """
 Module: 0x03. Log Parsing
 """
+import re
 import sys
 
 
@@ -28,16 +29,32 @@ if __name__ == "__main__":
         '500': 0
     }
     line_count = 0
+
+    patterns = (
+        r'\s*(?P<ip>\S+)\s*',
+        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
+        r'\s*"(?P<request>[^"]*)"\s*',
+        r'\s*(?P<status_code>\S+)',
+        r'\s*(?P<file_size>\d+)'
+    )
     try:
         while True:
             for line in sys.stdin:
                 line_count += 1
-                # print(line_count)
-                parts = line.split(' ')
-                if len(parts) == 9:
-                    file_size = parts[8]
+                log_fmt = '{}\\-{}{}{}{}\\s*'.format(
+                    patterns[0], patterns[1],
+                    patterns[2], patterns[3],
+                    patterns[4])
+                resp_match = re.fullmatch(log_fmt, line)
+                if resp_match is not None:
+                    code = resp_match.group('status_code')
+                    file_size = int(resp_match.group('file_size'))
+                    # print(line_count)
+                    # parts = line.split(' ')
+                    # if len(parts) == 9:
+                    #     file_size = parts[8]
                     total_file_size += int(file_size)
-                    code = parts[7]
+                    # code = parts[7]
                     status_codes[code] += 1
                 if line_count % 10 == 0:
                     print_stats(total_file_size, status_codes)
