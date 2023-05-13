661fab15b2d316564b3108345738a43d027802f2
Peter Chibunna <peterchibunna@gmail.com>
2023-05-13 17:29:30 +0100

    check if code in

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -30,34 +30,32 @@ if __name__ == "__main__":
     }
     line_count = 0
 
-    patterns = (
-        r'\s*(?P<ip_addr>\S+)\s*',
-        r'\s*\[(?P<request_date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
-        r'\s*"(?P<request_url>[^"]*)"\s*',
-        r'\s*(?P<http_code>\S+)',
-        r'\s*(?P<response_size>\d+)'
-    )
+    # patterns = (
+    #     r'\s*(?P<ip_addr>\S+)\s*',
+    #     r'\s*\[(?P<request_date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
+    #     r'\s*"(?P<request_url>[^"]*)"\s*',
+    #     r'\s*(?P<http_code>\S+)',
+    #     r'\s*(?P<response_size>\d+)'
+    # )
     try:
         while True:
             for line in sys.stdin:
                 line_count += 1
-                line_format = '{}\\-{}{}{}{}\\s*'.format(
-                    patterns[0], patterns[1],
-                    patterns[2], patterns[3],
-                    patterns[4])
-                regex_match = re.fullmatch(line_format, line)
-                if regex_match is not None:
-                    code = regex_match.group('http_code')
-                    file_size = int(regex_match.group('response_size'))
-                    # parts = line.split(' ')
-                    # if len(parts) == 9:
-                    #     file_size = parts[8]
+                # line_format = '{}\\-{}{}{}{}\\s*'.format(
+                #     patterns[0], patterns[1],
+                #     patterns[2], patterns[3],
+                #     patterns[4])
+                # regex_match = re.fullmatch(line_format, line)
+                # if regex_match is not None:
+                #     code = regex_match.group('http_code')
+                #     file_size = int(regex_match.group('response_size'))
+                parts = line.split(' ') # do not use this test again
+                if len(parts) == 9:
+                    file_size = parts[8]
                     total_file_size += int(file_size)
-                    # code = parts[7]
+                    code = parts[7]
                     if code in status_codes:
                         status_codes[code] += 1
-                # else:
-                #     print_stats(total_file_size, status_codes)
                 if line_count % 10 == 0:
                     print_stats(total_file_size, status_codes)
             # sys.stdin.flush()
