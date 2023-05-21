406262be5ab4fa458844b86df6bfac1db1fb918a
Peter Chibunna <peterchibunna@gmail.com>
2023-05-21 21:02:12 +0100

    Task #0.k

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,27 +5,27 @@ Module: 0x04. UTF-8 Validation
 
 
 def validUTF8(data):
-    """Test that all the datum in `data` is a valid UTF-8 encoding.
     """
-    num_bytes = 0
+    Return: True if data is a valid UTF-8 encoding, else return False.
+    """
+    n_bytes = 0
     for num in data:
-        binary_representation = format(num, '#010b')[-8:]
-        if num_bytes == 0:
-            for bit in binary_representation:
+        bin_rep = format(num, '#010b')[-8:]
+        if n_bytes == 0:
+            for bit in bin_rep:
                 if bit == '0':
                     break
-                num_bytes += 1
+                n_bytes += 1
 
-            if num_bytes == 0:
+            if n_bytes == 0:
                 continue
 
-            if num_bytes == 1 or num_bytes > 4:
+            if n_bytes == 1 or n_bytes > 4:
                 return False
         else:
-            if not (binary_representation[0] == '1'
-                    and binary_representation[1] == '0'):
+            if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
                 return False
 
-        num_bytes -= 1
+        n_bytes -= 1
 
-    return num_bytes == 0
+    return n_bytes == 0
