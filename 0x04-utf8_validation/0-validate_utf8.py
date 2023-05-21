28a6a5b6dc0d438c6a1b73727c2c0e73e8fe9e8e
Peter Chibunna <peterchibunna@gmail.com>
2023-05-21 07:43:41 +0100

    add file for task 0: test correction

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """
-Main module: 0x04. UTF-8 Validation
+Main module:
+0x04. UTF-8 Validation
 """
 from typing import List
 
 
 def validUTF8(data: List) -> bool:
     """Test if data is a valid utf8 and return True or False
+    For each data in the data, convert to binary and test the bits
+    to make sure t
     """
     num_of_bytes = 0
     for num in data:
@@ -15,7 +18,7 @@ def validUTF8(data: List) -> bool:
             for bit in binary_representation:
                 if bit == '0':
                     break
-                num_of_bytes += 1
+                num_of_bytes = num_of_bytes + 1
 
             if num_of_bytes == 0:
                 continue
@@ -26,6 +29,6 @@ def validUTF8(data: List) -> bool:
             if not (binary_representation[0] == '1' and
                     binary_representation[1] == '0'):
                 return False
-        num_of_bytes -= 1
+        num_of_bytes = num_of_bytes - 1
 
     return num_of_bytes == 0
