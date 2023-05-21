e3b53bec24e8d09c1bc15f87554fb84d8ad8b398
Peter Chibunna <peterchibunna@gmail.com>
2023-05-21 21:10:57 +0100

    Refactor and cleanup

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,15 +4,21 @@ Module: 0x04. UTF-8 Validation
 """
 
 
+def bin8(x):
+    """Get 'x' expressed in binary with 8-width padding
+    """
+    return ''.join(reversed([str((x >> i) & 1) for i in range(8)]))
+
+
 def validUTF8(data):
     """
     Return: True if data is a valid UTF-8 encoding, else return False.
     """
     n_bytes = 0
     for num in data:
-        bin_representation = format(num, '#010b')[-8:]
+        bin_represent = bin8(num)
         if n_bytes == 0:
-            for bit in bin_representation:
+            for bit in bin_represent:
                 if bit == '0':
                     break
                 n_bytes += 1
@@ -23,7 +29,7 @@ def validUTF8(data):
             if n_bytes == 1 or n_bytes > 4:
                 return False
         else:
-            if not (bin_representation[0] == '1' and bin_representation[1] == '0'):
+            if not (bin_represent[0] == '1' and bin_represent[1] == '0'):
                 return False
 
         n_bytes -= 1
