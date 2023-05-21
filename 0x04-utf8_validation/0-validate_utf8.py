577733fdacec1e4ac045e7302e3a0975983cf7a7
Peter Chibunna <peterchibunna@gmail.com>
2023-05-21 21:32:41 +0100

    Task 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,6 +2,7 @@
 """
 Module: 0x04. UTF-8 Validation
 """
+import sys
 
 
 def bin8(x):
@@ -18,9 +19,31 @@ def validUTF8(data):
     can_decode = True
     for val in data:
         try:
-            val.to_bytes(800, 'big').decode('utf8')
+            val.to_bytes(800, sys.byteorder).decode('utf8')
         except UnicodeDecodeError:
             can_decode = False
         except OverflowError:
             can_decode = True
     return can_decode
+
+    n_bytes = 0
+    for num in data:
+        bin_represent = bin8(num)
+        if n_bytes == 0:
+            for bit in bin_represent:
+                if bit == '0':
+                    break
+                n_bytes += 1
+
+            if n_bytes == 0:
+                continue
+
+            if n_bytes == 1 or n_bytes > 4:
+                return False
+        else:
+            if not (bin_represent[0] == '1' and bin_represent[1] == '0'):
+                return False
+
+        n_bytes -= 1
+
+    return n_bytes == 0
