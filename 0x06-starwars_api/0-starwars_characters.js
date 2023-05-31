892db7a163f0b8dc445a3bef56f5cf9266abe568
Peter Chibunna <peterchibunna@gmail.com>
2023-05-31 20:11:06 +0100

    Task #0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 const id = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/';
+const url = 'https://swapi-api.alx-tools.com/api/films/';
 request.get(url + id, function (error, res, body) {
   if (error) {
     console.log(error);
