4696e0493356ef980a233a279e0484be644a8c9a
Peter Chibunna <peterchibunna@gmail.com>
2023-06-08 22:14:12 +0100

    Implement custom zip function

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,12 +3,25 @@
 """
 
 
+def myzip(*iterables):
+    """Let me take a shot at implementing my own zip function so I can do
+    away with the builtin zip method. Hahaha
+    """
+    new_list = []
+    x = len(iterables)
+    n = len(iterables[0])
+    for i in range(n):
+        new_item = tuple(iterable[i] for iterable in iterables)
+        new_list.append(new_item)
+    return new_list
+
+
 def rotate_2d_matrix(matrix):
     """Rotates an m by n 2D matrix in place.
     """
     if type(matrix) == list and len(matrix) > 0 and all(
             map(lambda x: type(x) == list, matrix)):
-        rotated = [list(l[::-1]) for l in zip(*matrix)]
+        rotated = [list(l[::-1]) for l in myzip(*matrix)]
         for idx, i in enumerate(rotated):
             matrix[idx] = i
     else:
